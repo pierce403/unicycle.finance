58b175e563d15cc45e67097c08872df09a6c3f21
Dean Pierce <pierce403@gmail.com>
2020-03-06 20:31:07 -0800

    some slight cosmetic tweaks (and making it work)

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-let uniswapData = [
-    [65, 59, 80, 81, 56, 55, 40]
-]
+let uniswapData = [];
+
+let uniswapDays = [];//=["1/22", "1/23", "1/24", "1/25", "1/26", "1/27", "1/28", "1/29", "2/1", "2/2", "2/3", "2/4", "2/5", "2/6"];
+let thisDate = new Date();
+for(let x=0;x<14;++x)
+{
+    uniswapDays.unshift(thisDate.getMonth()+"/"+thisDate.getDate());
+    thisDate.setDate(thisDate.getDate() - 1);
+}
 
 let scope;
+let displayedCount=0;
 
 doGraphQL();
 
 angular.module("app", ["chart.js"]).controller("LineCtrl", function ($scope) {
     scope=$scope;
-    $scope.labels = ["M", "T", "W", "Th", "F", "Sa", "S", "M", "T", "W", "Th", "F", "Sa", "S"];
+    $scope.labels = uniswapDays;
     $scope.series = ['Series A', 'Series B'];
     $scope.data = uniswapData;
     $scope.onClick = function (points, evt) {
@@ -40,7 +47,41 @@ angular.module("app", ["chart.js"]).controller("LineCtrl", function ($scope) {
 
 function doGraphQL() {
     var query = `query {
-        exchangeDayDatas(where:{exchangeAddress: "0x97dec872013f6b5fb443861090ad931542878126"}, orderBy:date, orderDirection:desc, first:14)
+        exchangeDayDatas(where:{date: 1583452800}, orderBy:ethVolume, orderDirection:desc, first:12)
+                  {
+                    exchangeAddress
+                  }
+                }
+      `;
+
+    fetch('https://api.thegraph.com/subgraphs/name/graphprotocol/uniswap', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            },
+            body: JSON.stringify({
+                query,
+            })
+        })
+        .then(r => r.json())
+        //.then(data => console.log('data returned:', data));
+        .then(function (data) {
+            console.log('data returned:', data);
+
+            uniswapData[0] = [];
+            for (entry in data['data']['exchangeDayDatas']) {
+                getProfits(data['data']['exchangeDayDatas'][entry]['exchangeAddress'],entry);
+                console.log("addr is "+data['data']['exchangeDayDatas'][entry]['exchangeAddress']);
+            }
+        }).then(function () {
+            //scope.$apply();
+        });
+}
+
+function getProfits(addr,number) {
+    var query = `query {
+        exchangeDayDatas(where:{exchangeAddress: "`+addr+`"}, orderBy:date, orderDirection:desc, first:14)
           {
             id
             date
@@ -66,12 +107,22 @@ function doGraphQL() {
             console.log('data returned:', data);
 
             console.log("awesome");
-            uniswapData[0] = [];
+            //uniswapDays=[];
+
+            uniswapData[number] = [];
             for (entry in data['data']['exchangeDayDatas']) {
-                console.log(data['data']['exchangeDayDatas'][entry]['ethVolume']);
-                uniswapData[0][entry] = data['data']['exchangeDayDatas'][entry]['ethVolume']
+                let vol = data['data']['exchangeDayDatas'][entry]['ethVolume'];
+                let liq = data['data']['exchangeDayDatas'][entry]['ethBalance']*2;
+                let ryr = vol*0.003/liq*100*365 // relative yearly returns;
+                console.log(ryr);
+                uniswapData[number].unshift(ryr);
             }
         }).then(function () {
-            scope.$apply();
+            //scope.$apply();
+            displayedCount++;
+            if(displayedCount===uniswapData.length)
+            {
+                scope.$apply();
+            }
         });
 }
