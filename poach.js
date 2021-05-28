2d2c04a159cf3e717e8585a8c9aa30d347c3c46b
Dean Pierce <pierce403@gmail.com>
2021-05-27 19:06:10 -0700

    some old poach stuff

---
+++ poach.js
@@ -0,0 +1,152 @@
+let uniswapData = [];
+
+let uniswapDays = [];
+let seriesNames = [];
+let thisDate = new Date();
+for(let x=0;x<14;++x)
+{
+    uniswapDays.unshift(thisDate.getMonth()+1+"/"+thisDate.getDate());
+    thisDate.setDate(thisDate.getDate() - 1);
+}
+
+let scope;
+let displayedCount=0;
+
+doGraphQL();
+
+angular.module("app", ["chart.js"]).controller("LineCtrl", function ($scope) {
+    scope=$scope;
+    $scope.labels = uniswapDays;
+    $scope.series = seriesNames;
+    $scope.data = uniswapData;
+    $scope.onClick = function (points, evt) {
+        console.log(points, evt);
+    };
+    $scope.datasetOverride = [{
+        yAxisID: 'y-axis-1'
+    }];
+    $scope.options = {
+        scales: {
+            yAxes: [{
+                    id: 'y-axis-1',
+                    type: 'linear',
+                    display: true,
+                    position: 'left'
+                }
+            ]
+        }
+    };
+});
+
+function doGraphQL() {
+    let rightnow = Math.round(new Date().getTime()/1000);
+    let today = rightnow - rightnow%86400;
+    
+    var query = `query {
+        pairDayDatas(first: 10, where:{date:`+today+`}, orderBy:dailyVolumeUSD, orderDirection:desc) {
+            pairAddress
+            token0{
+                symbol
+              }
+              token1{
+                symbol
+              }
+        }
+    }`;
+
+    fetch('https://api.thegraph.com/subgraphs/name/ianlapham/uniswapv2', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            },
+            body: JSON.stringify({
+                query,
+            })
+        })
+        .then(r => r.json())
+        //.then(data => console.log('data returned:', data));
+        .then(function (data) {
+            console.log('data returned:', data);
+
+            uniswapData[0] = [];
+            console.log(data);
+            for (entry in data['data']['pairDayDatas']) {
+                let token0 = data['data']['pairDayDatas'][entry]['token0']['symbol'];
+                let token1 = data['data']['pairDayDatas'][entry]['token1']['symbol'];
+                seriesNames.push(token0+'/'+token1);
+                console.log(token0+'/'+token1)
+                getProfits(data['data']['pairDayDatas'][entry]['pairAddress'],entry,token0+'/'+token1);
+                console.log("addr is "+data['data']['pairDayDatas'][entry]['pairAddress']);
+            }
+        }).then(function () {
+            //scope.$apply();
+        });
+}
+
+function getProfits(addr,number) {
+    var query = `query {
+            pairDayDatas(where:{pairAddress: "`+addr+`"}, orderBy:date, orderDirection:desc, first:14)
+              {
+                id
+                date
+                dailyVolumeUSD
+                reserveUSD
+                token0{
+                    symbol
+                  }
+                  token1{
+                    symbol
+                  }
+              }
+            }
+      `;
+
+    fetch('https://api.thegraph.com/subgraphs/name/ianlapham/uniswapv2', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            },
+            body: JSON.stringify({
+                query,
+            })
+        })
+        .then(r => r.json())
+        //.then(data => console.log('data returned:', data));
+        .then(function (data) {
+            console.log('data returned:', data);
+
+            try
+            {
+            let token0 = data['data']['pairDayDatas'][entry]['token0']['symbol'];
+            let token1 = data['data']['pairDayDatas'][entry]['token1']['symbol'];
+            console.log("pair: "+token0+'/'+token1);
+            }
+            catch(err)
+            {
+                console.log(err);
+            }
+            //uniswapDays=[];
+
+            uniswapData[number] = [];
+            for (entry in data['data']['pairDayDatas']) {
+                let vol = data['data']['pairDayDatas'][entry]['dailyVolumeUSD'];
+                let liq = data['data']['pairDayDatas'][entry]['reserveUSD'];
+                let ryr = vol*0.003/liq*100*365 // relative yearly returns;
+                console.log(ryr);
+                uniswapData[number].unshift(ryr);
+            }
+
+            // pad some zeroes for the really new stuff
+            while(uniswapData[number].length<14)uniswapData[number].unshift(0);
+
+        }).then(function () {
+            //scope.$apply();
+            displayedCount++;
+            if(displayedCount===uniswapData.length)
+            {
+                scope.$apply();
+            }
+        });
+}
---
+++ uv1list.py
@@ -0,0 +1,37 @@
+import os
+import sys
+from web3 import Web3, HTTPProvider
+
+infura=None
+
+try:
+  infura = Web3(HTTPProvider(os.environ['WEB3']))
+  erc20abi='''[{"inputs":[{"internalType":"uint256","name":"initialSupply","type":"uint256"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"spender","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"subtractedValue","type":"uint256"}],"name":"decreaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"addedValue","type":"uint256"}],"name":"increaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"recipient","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"sender","type":"address"},{"internalType":"address","name":"recipient","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"}]'''
+
+  uv1factoryAbi='''[{"name":"NewExchange","inputs":[{"type":"address","name":"token","indexed":true},{"type":"address","name":"exchange","indexed":true}],"anonymous":false,"type":"event"},{"name":"initializeFactory","outputs":[],"inputs":[{"type":"address","name":"template"}],"constant":false,"payable":false,"type":"function","gas":35725},{"name":"createExchange","outputs":[{"type":"address","name":"out"}],"inputs":[{"type":"address","name":"token"}],"constant":false,"payable":false,"type":"function","gas":187911},{"name":"getExchange","outputs":[{"type":"address","name":"out"}],"inputs":[{"type":"address","name":"token"}],"constant":true,"payable":false,"type":"function","gas":715},{"name":"getToken","outputs":[{"type":"address","name":"out"}],"inputs":[{"type":"address","name":"exchange"}],"constant":true,"payable":false,"type":"function","gas":745},{"name":"getTokenWithId","outputs":[{"type":"address","name":"out"}],"inputs":[{"type":"uint256","name":"token_id"}],"constant":true,"payable":false,"type":"function","gas":736},{"name":"exchangeTemplate","outputs":[{"type":"address","name":"out"}],"inputs":[],"constant":true,"payable":false,"type":"function","gas":633},{"name":"tokenCount","outputs":[{"type":"uint256","name":"out"}],"inputs":[],"constant":true,"payable":false,"type":"function","gas":663}]'''
+
+
+  uv1fAddress = '0xc0a47dFe034B400B47bDaD5FecDa2621de6c4d95'
+  #gstToken = infura.eth.contract(address=uv1fAddress,abi=erc20aabi)
+  uv1 = infura.eth.contract(address=uv1fAddress,abi=uv1factoryAbi)
+
+except:
+  print("[!] WARNING: INVALID HTTPProvider ('WEB3' env variable)")
+  print("[!] sign up to get an API token from infura.io")
+  print("[!] backend web3 queries disabled")
+  #sys.exit("NOPE")
+
+for x in range(0,10):
+
+  addr = uv1.functions.getTokenWithId(x).call()
+  print(addr)
+
+
+def tokencount(address):
+  if infura is None:
+    return 0
+
+  decimals = gstToken.functions.decimals().call()
+  
+  return int(gstToken.functions.balanceOf(address).call())/10**decimals
+
